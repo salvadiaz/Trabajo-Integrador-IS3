f1f0958ce87e6ec282fb0222c910925dafd05e47
salvadiaz <salvadiazpesci@gmail.com>
2020-11-23 20:35:53 -0300

    some changes

--- result/server.js
+++ result/server.js
@@ -23,7 +23,6 @@ io.sockets.on('connection', function (socket) {
   });
 });
 
-const REDIS_URI = process.env.REDISCLOUD_URL
 const DB_USER = process.env.POSTGRES_USER;
 const DB_PASS =  process.env.POSTGRES_PASS;
 const DB_HOST = process.env.POSTGRES_HOST;
