41926b1a77ffcdb44c1e63cc3c4797419f9433c6
salvadiaz <salvadiazpesci@gmail.com>
2020-11-24 20:13:18 -0300

    tests

---
+++ vote/tests/test_app.py
@@ -0,0 +1,38 @@
+from app import app
+import requests
+import unittest
+
+
+class UnitTest(unittest.TestCase):
+    url = 'https://is3-vote.herokuapp.com/'
+
+    def test_get_status_200(self):
+        response = requests.get(self.url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_vote_response_code(self):
+        payload = {'vote': 'a'}
+        headers = {
+            'Cookie': 'voter_id=39e29be59e1d5c6'
+        }
+        response = requests.post(self.url, headers=headers, data=payload)
+        self.assertEqual(response.status_code, 200)
+        # Deberia ser 201
+
+    def test_incomplete_vote_status_400(self):
+        response = requests.post(self.url)
+        self.assertEqual(response.status_code, 400)  # Bad Req
+
+    def test_get_voter_id_cookie(self):
+        valid_cookie = '39e29be59e1d5c6'
+        payload = {'vote': 'a'}
+        headers = {
+            'Cookie': 'voter_id=39e29be59e1d5c6'
+        }
+        response = requests.post(self.url, headers=headers, data=payload)
+        self.assertEqual(response.cookies["voter_id"], valid_cookie)
+
+    def test_set_new_cookie(self):
+        payload = {'vote': 'a'}
+        response = requests.post(self.url, data=payload)
+        assert response.cookies["voter_id"] is not None
